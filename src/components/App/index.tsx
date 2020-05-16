8c07c259b9b1c78c33f76ebb9522c533b9a0ca43
eatthatpie <eatthatpie.dev@gmail.com>
2020-05-16 09:47:46 +0200

    quick refactor

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { selectCurrentTheme } from '@/store/selectors';
 import { ThemeProvider } from 'styled-components';
 import * as S from './styles';
 import theme from '@/styles/theme';
 
 function App() {
-  const currentTheme = useSelector(state => state.settings.theme);
+  const currentTheme = useSelector(selectCurrentTheme());
   const currentThemeObject = theme[currentTheme];
 
   return (
